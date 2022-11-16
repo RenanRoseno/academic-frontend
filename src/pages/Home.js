4260c37b531611f8ec6cf24faeb698615ddab090
Renan Roseno <rosenorenan@gmail.com>
2022-11-16 11:35:34 -0300

    Add new table style, buttons and updating package.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,67 @@
 import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import SaveIcon from '@mui/icons-material/Save';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: "id", headerName: "#", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
+    field: "name",
+    headerName: "Nome",
+    width: 250,
     editable: true,
   },
   {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
+    field: "email",
+    headerName: "Email",
+    width: 250,
     editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
+    field: "register",
+    headerName: "Identificação",
+    width: 250,
     editable: true,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "phone",
+    headerName: "Telefone",
+    width: 200,
+    editable: true,
   },
+  // {
+  //   field: "fullName",
+  //   headerName: "Full name",
+  //   description: "This column has a value getter and is not sortable.",
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params) =>
+  //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  // },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 
 export default function Home() {
+  const [professors, setProfessors]=useState([])
+
+  useEffect(() => {
+    loadProfessors();
+  }, []);
+
+
   const [checkboxSelection, setCheckboxSelection] = React.useState(true);
   
   let selectedParam = null
 
   const onRowsSelectionHandler = (ids) => {
-    const selectedRowsData = ids.map((id) => rows.find((row) => row.id === id));
+    const selectedRowsData = ids.map((id) => professors.find((row) => row.id === id));
     if(selectedRowsData){
       selectedParam = selectedRowsData[0];
     } else {
@@ -63,32 +71,79 @@ export default function Home() {
     console.log(selectedRowsData);
   };
   
+  const loadProfessors= async () => {
+    const result = await axios.get("http://localhost:8080/professors/")
+    setProfessors(result.data)
+    console.log(result.data);
+
+  }
   const teste = (params) => {
     console.log(selectedParam);
-    return selectedParam ? params.row.id == selectedParam.id : true;
+    return selectedParam ? params.row.id === selectedParam.id : true;
     
   }
   
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="container">
+          
       <Box sx={{ height: 400, width: "100%" }}>
-      <Button
+      <h3>Gerenciamento de professores</h3>
+      {/* <Button
         sx={{ mb: 2 }}
         onClick={() => setCheckboxSelection(!checkboxSelection)}
       >
         Toggle checkbox selection
-      </Button>
+      </Button> */}
         <DataGrid
-          rows={rows}
+          rows={professors}
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
           checkboxSelection={checkboxSelection}
           experimentalFeatures={{ newEditingApi: true }}
           onSelectionModelChange={(ids) => onRowsSelectionHandler(ids)}
-          isRowSelectable={(params) => teste(params)}
+          // isRowSelectable={(params) => teste(params)}
         />
       </Box>
+      <div class="d-flex justify-content-end mt-5">
+     <div class="row ">
+     <div class="col-md-3 ml-md-auto">  <Button variant="btn btn-light" onClick={handleShow}>
+        <AddIcon />
+      </Button></div>
+      <div class="col-md-3 ml-md-auto"><Button variant="btn btn-light" onClick={handleShow}>
+        <EditIcon />
+      </Button></div>
+      <div class="col-md-3 ml-md-auto"><Button variant="btn btn-light" onClick={handleShow}>
+        <DeleteForeverIcon />
+      </Button></div>
+      </div>
+    
+      {/* &nbsp;
+      <Button variant="btn btn-light" onClick={handleShow}>
+        <SaveIcon />
+      </Button> */}
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
     </div>
   );
 }
